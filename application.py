75a194b9af243df509130ae65ec71e4f3fb1cf42
Xin Bian <bianxin2006@gmail.com>
2019-06-22 17:20:10 -0400

    change to py3

--- application.py
+++ application.py
@@ -86,7 +86,7 @@ def gconnect():
     if result['issued_to'] != CLIENT_ID:
         response = make_response(
             json.dumps("Token's client ID does not match app's."), 401)
-        print "Token's client ID does not match app's."
+        print("Token's client ID does not match app's.")
         response.headers['Content-Type'] = 'application/json'
         return response
 
@@ -128,7 +128,7 @@ def gconnect():
     output += ' "style = "width: 300px; height: 300px;border-radius: 150px;'\
         '-webkit-border-radius: 150px;-moz-border-radius: 150px;"> '
     flash("you are now logged in as %s" % login_session['username'])
-    print "done!"
+    print("done!")
     return output
 
 
